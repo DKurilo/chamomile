a3eb4c81ba1fc40c35f4c9bd3b1357c523681dee
Dmitry Kurilo <dima@kurilo.su>
2017-09-15 16:27:37 -0400

    minor fixes

--- chamomile.sh
+++ chamomile.sh
@@ -2,6 +2,6 @@
 
 TEMPLATE=`wc -l $1 | grep -o -E '[0-9]+' | sed 's/[0-9]/0/ig;s/.$//'`
 CHOICES=`cat $1 | sed '/.*/{=;d;}' | sed ':a;N;$!ba;s/\n/|/g' | sed "s/\<[0-9]\>/${TEMPLATE}&/ig"`
-FOUND=`cat /dev/urandom | strings --bytes 1 | tr -d "\t i" | grep --line-buffered -m 1 -o -E "(${CHOICES})"`
+FOUND=`cat /dev/urandom | strings --bytes 1 | tr -d "\t " | grep --line-buffered -m 1 -o -E "(${CHOICES})"`
 sed "${FOUND}!d" $1
 
