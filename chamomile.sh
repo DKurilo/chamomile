9cb7ebae0e3e351a4a37f47d40becc84fb54e76f
Dmitry Kurilo <dima@kurilo.su>
2017-09-16 10:41:32 -0400

    fixed errors, added illiada

--- chamomile.sh
+++ chamomile.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-TEMPLATE=`wc -l $1 | grep -o -E '[0-9]+' | sed 's/[0-9]/0/ig;s/.$//'`
-CHOICES=`cat $1 | sed '/.*/{=;d;}' | sed ':a;N;$!ba;s/\n/|/g' | sed "s/\<[0-9]\>/${TEMPLATE}&/ig"`
-FOUND=`cat /dev/urandom | strings --bytes 1 | tr -d "\t " | grep --line-buffered -m 1 -o -E "(${CHOICES})"`
+MAXNUMB=`wc -l $1 | grep -o -E '[0-9]+' | wc -m`
+TEMPLATE=`expr $MAXNUMB - 1`
+CHOICES=`cat $1 | sed '/.*/{=;d;}' | awk "{\\$1 = sprintf(\"%0${TEMPLATE}d\", \\$1); print}" | sed ':a;N;$!ba;s/\n/\\\|/g'`
+FOUND=`cat /dev/urandom | strings --bytes 1 | tr -d "\t " | grep --line-buffered -m 1 -o "(${CHOICES})"`
 sed "${FOUND}!d" $1
 
